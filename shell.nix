cda1caa9271c213eadc360858f6a3fe93645f5ee
Ellie Hermaszewska <git@monoid.al>
2021-11-25 22:03:33 +0800

    update nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { nixpkgsSrc ? builtins.fetchTarball {
   url =
-    "https://github.com/nixos/nixpkgs/archive/83d907fd760d9ee4f49b4b7e4b1c6682f137b573.tar.gz"; # nixos-unstable
-  sha256 = "0qc8gfkbga7v5p8r6h2hbdi8bjl3piikdwcbb9fff5cjnxi7v7qa";
+    "https://github.com/nixos/nixpkgs/archive/39cb89ef2ffcbef8d2ac2d52267ac69121a6becb.tar.gz"; # nixos-unstable
+  sha256 = "1x9vzpay56ap4hgfq1giz00050crdngv5jkxgkxzx276mzhw93na";
 }, pkgs ? import nixpkgsSrc { } }:
 
 with pkgs;
